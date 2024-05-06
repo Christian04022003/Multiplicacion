75d93de4a6df14c91db6fb0e82fa10633e287fdc
Christian Hernandez <zs21017296@estudiantes.uv.mx>
2024-05-06 13:28:41 -0600

    Primer funcionalidad de mostrar trablas y menu

---
+++ TablaMultiplicar/src/main/java/christian/uv/TablaMultiplicar/TablaMultiplicarApplication.java
@@ -0,0 +1,49 @@
+package christian.uv.TablaMultiplicar;
+
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablaMultiplicarApplication {
+
+	public class Main {
+		public static void main(String[] args) {
+	  
+		  monstrarUI();
+		}
+	  
+		public static void multiplicador(int numero) {
+		  System.out.println("___________________");
+		  for (int index = 0; index < 10; index++) {
+			System.out.println(numero + " x " + (index + 1) + " = " + (numero * (index + 1)));
+	  
+		  }
+	  
+		}
+	  
+		public static void monstrarUI() {
+		  Scanner in = new Scanner(System.in);
+		  System.out.println("--------------------");
+		  System.out.println("1 - Mostrar una tabla");
+		  System.out.println("2 - Mostrar del 2 al 5");
+		  System.out.println("3 - Salir");
+		  System.out.println("--------------------");
+		  int numero = in.nextInt();
+	  
+		  switch (numero) {
+			case 1:
+	  
+			  System.out.println("Ingresa el número de la tabla que quieres: ");
+			  int numeroTabla = in.nextInt();
+	  
+			  multiplicador(numeroTabla);
+	  
+			  break;
+		  }
+		}
+	  
+	  }
+
+}
---
+++ TablaMultiplicar/src/test/java/christian/uv/TablaMultiplicar/TablaMultiplicarApplicationTests.java
@@ -0,0 +1,13 @@
+package christian.uv.TablaMultiplicar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablaMultiplicarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
